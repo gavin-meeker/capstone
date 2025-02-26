8b0fdeb3ca1ca578579712fe84226932b1dbe8d9
Gavin Meeker <gmeeke99@gmail.com>
2025-02-25 21:15:35 -0500

    refactor iocTable endpoint

--- server/handlers/iocTable.js
+++ server/handlers/iocTable.js
@@ -6,18 +6,22 @@ import { theCount } from "../theCountApi.js";
  */
 export default async function iocTable(req, res) {
   //TODO: need to refactor to add error handling
-  //TODO: need to refactor to decompose logic
-  const {
-    data: { data },
-  } = await theCount.get("/oil/1.2.3.4");
+  try {
+    const {
+      data: { data: oilData },
+    } = await theCount.get("/oil/1.2.3.4");
 
-  let returnItem = {};
+    const responseObject = buildReturnObject(oilData);
 
-  data.forEach((IOC) => {
-    Object.hasOwn(returnItem, IOC.oil)
-      ? returnItem[IOC.oil]++
-      : (returnItem[IOC.oil] = 1);
-  });
+    res.send(responseObject);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
 
-  res.send(returnItem);
+function buildReturnObject(oilData) {
+  return oilData.reduce((acc, IOC) => {
+    acc[IOC.oil] = (acc[IOC.oil] || 0) + 1;
+    return acc;
+  }, {});
 }
