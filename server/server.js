babb59112c13286b72dc5a9f2146d01a1bc92680
Gavin Meeker <gmeeke99@gmail.com>
2025-02-27 21:54:52 -0500

    use cors in server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import router from "./router.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.text());
+app.use(cors());
 
 app.use("/v1", router);
 
