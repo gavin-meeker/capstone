af3c4908ec44410187bf7967625ce63e7341b0c2
Gavin Meeker <gmeeke99@gmail.com>
2025-03-12 05:40:41 -0400

    organize classes

--- client/src/components/IocTable.jsx
+++ client/src/components/IocTable.jsx
@@ -4,7 +4,7 @@ import IocTableRow from "./IocTableRow.jsx";
 const IocTable = ({ iocArray }) => {
   return (
     <>
-      <div className="h-full mx-auto w-5/6 overflow-scroll">
+      <div className="mx-auto h-full w-5/6">
         <table className="w-full min-w-max table-auto text-left">
           <thead>
             <tr>
@@ -25,7 +25,7 @@ const IocTable = ({ iocArray }) => {
                 key={ioc.threat.indicator.description}
                 className="hover:bg-gray-50"
               >
-                <td className="pl-4 py-4 border-b border-gray-300">
+                <td className="border-b border-gray-300 py-4 pl-4">
                   <Typography>{ioc.threat.indicator.description}</Typography>
                 </td>
               </tr>
