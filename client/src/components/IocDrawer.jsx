94cd8796f333019a0d510a88fce2f03100bb5fab
Gavin Meeker <gmeeke99@gmail.com>
2025-03-15 13:40:07 -0400

    add showing ioc type in drawer

--- client/src/components/IocDrawer.jsx
+++ client/src/components/IocDrawer.jsx
@@ -18,26 +18,31 @@ const IocDrawer = ({ closeDrawer, currentIoc, isOpen }) => {
         className="p-4"
         overlayProps={{ className: "bg-black/25 shadow-none" }}
       >
-        <div className="mb-6 flex items-center justify-between">
-          <Typography variant="h3" color="blue-gray">
-            {truncateString(currentIoc)}
+        <div className="mb-6">
+          <div className="flex items-center justify-between">
+            <Typography variant="h3" color="blue-gray" className="mb-0.5">
+              {truncateString(currentIoc.threat.indicator.description)}
+            </Typography>
+            <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </IconButton>
+          </div>
+          <Typography variant="small" color="blue-gray" className="block">
+            [{currentIoc.threat.indicator.type}]
           </Typography>
-          <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </IconButton>
         </div>
         <Typography variant="h5" color="gray" className="mb-8 pr-4 font-normal">
           Security Logs
