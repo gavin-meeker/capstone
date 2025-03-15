f582685783f1e1ed4e6c126054f8f42473e263bd
Gavin Meeker <gmeeke99@gmail.com>
2025-03-15 14:07:41 -0400

    add theCount Get passthrough endpoint

--- server/router.js
+++ server/router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import iocTable from "./handlers/iocTable.js";
 import extract from "./handlers/extract.js";
+import theCountEndpoint from "./handlers/theCountEndpoint.js";
 
 const router = Router();
 
 router.post("/ioc-table", iocTable);
-
 router.post("/extract", extract);
+router.post("/thecount/*", theCountEndpoint);
 
 export default router;
