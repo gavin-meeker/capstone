f032a4f65749016746a469b704cb481cb68d1013
Gavin Meeker <gmeeke99@gmail.com>
2025-03-15 14:08:41 -0400

    extract passivedns into own component

---
+++ client/src/components/PassiveDNSDrawer.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@material-tailwind/react";
+
+const PassiveDNSDrawer = () => {
+  return (
+    <>
+      <Typography variant="h5" color="gray" className="mb-8 pr-4 font-normal">
+        Passive DNS
+      </Typography>
+    </>
+  );
+};
+
+export default PassiveDNSDrawer;
