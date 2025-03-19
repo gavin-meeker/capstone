c92c7434d29f83819dbcb853f66eca414dcd29a2
Gavin Meeker <gmeeke99@gmail.com>
2025-03-19 19:50:23 -0400

    revert to tailwind 3

---
+++ client/tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// @ts-ignore material ui makes you do this
+const withMT = require("@material-tailwind/react/utils/withMT");
+export default withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
