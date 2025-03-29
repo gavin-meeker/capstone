2bb6b1c9e21d5b0065c35be99fe1d3605db9c256
Gavin Meeker <gmeeke99@gmail.com>
2025-03-29 14:21:46 -0400

    add logging for express server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import router from "./router.js";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(cors());
+// TODO: need to conditionally use morgan based on if environment is PROD or DEV
+app.use(morgan("dev"));
 app.use(express.text());
 app.use(express.json());
 
