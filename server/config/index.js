f7fadb525ce15de9d4b7c21ef7c2b84ec6311b5b
ericaransom <ericaransom@hotmail.com>
2025-04-10 19:47:28 -0400

    testing cross compatability

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,12 @@
 // TODO: need to have better deployment strategy for dev vs prod
-import dotnev from "dotenv";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
-const config = dotnev.config({
+const config = dotenv.config({
   path: path.join(__dirname, "../../.env.development.local"),
 });
 
--- server/theCountApi.js
+++ server/theCountApi.js
@@ -5,7 +5,7 @@ const port = process.env.COUNT_PORT || 7000;
 export const theCount = axios.create({
   baseURL: `http://localhost:${port}`,
   auth: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    username: process.env.API_USERNAME,  // Changed from USERNAME
+    password: process.env.API_PASSWORD,  // Changed from PASSWORD
   },
 });
